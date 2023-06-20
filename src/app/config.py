90cda08262b68fea88678def19673abb4b233a10
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2023-06-20 22:30:55 +0300

    fix(api/config):  rename ".env" file to ".env.local"

--- src/app/config.py
+++ src/app/config.py
@@ -5,13 +5,14 @@ class Settings(BaseSettings):
     """
     Settings for the application. Get settings from .env file.
     """
+
     SITE_DOMAIN: str = "api.innohassle.ru"
 
-    APP_VERSION: str = "0.1.0"
+    APP_VERSION: str
     APP_DESCRIPTION: str = "InNoHassle-Events API"
 
     class Config:
-        env_file = ".env"
+        env_file = ".env.local"
         env_file_encoding = "utf-8"
 
 
