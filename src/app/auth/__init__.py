5dcc50846df4b8654630f7afccfbcf34a9c30efe
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2023-06-21 20:55:05 +0300

    fix: authorization scheme is bearer

--- src/app/auth/__init__.py
+++ src/app/auth/__init__.py
@@ -2,23 +2,26 @@ __all__ = ["oauth", "router", "get_current_user_email"]
 
 from authlib.integrations.starlette_client import OAuth
 from fastapi import APIRouter, Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from starlette import status
 
 from src.app.auth.jwt import verify_token
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 oauth = OAuth()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+bearer_scheme = HTTPBearer(
+    scheme_name="Bearer token",
+    description="Your JSON Web Token (JWT)",
+)
 
 
-def get_current_user_email(data: str = Depends(oauth2_scheme)):
+def get_current_user_email(auth: HTTPAuthorizationCredentials = Depends(bearer_scheme)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
-    return verify_token(data, credentials_exception).email
+    return verify_token(auth.credentials, credentials_exception).email
 
 
 # Register all OAuth applications and routes
