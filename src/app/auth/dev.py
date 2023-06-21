546c041e7cb3779a139f4a72eac050e2519ae0f8
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2023-06-21 21:05:00 +0300

    chore: register auth routes only for enabled providers

--- src/app/auth/dev.py
+++ src/app/auth/dev.py
@@ -1,7 +1,6 @@
 __all__ = []
 
 from typing import Optional
-from fastapi import status, HTTPException
 from starlette.responses import RedirectResponse
 
 from src.app.auth import router
@@ -20,26 +19,14 @@ if enabled:
         "(otherwise, set ENVIRONMENT=production)."
     )
 
+    @router.get("/dev/login")
+    async def login_via_dev(user_id: Optional[str] = None):
+        url = redirect_uri
+        if user_id:
+            url += "?user_id=" + user_id
+        return RedirectResponse(redirect_uri, status_code=302)
 
-def check_enabled():
-    if not enabled:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Auth provider is not enabled",
-        )
-
-
-@router.get("/dev/login")
-async def login_via_dev(user_id: Optional[str] = None):
-    check_enabled()
-    url = redirect_uri
-    if user_id:
-        url += "?user_id=" + user_id
-    return RedirectResponse(redirect_uri, status_code=302)
-
-
-@router.get("/dev/token")
-async def auth_via_dev(user_id: Optional[str] = None) -> Token:
-    check_enabled()
-    user_id = user_id or settings.DEV_AUTH_EMAIL
-    return create_access_token(user_id)
+    @router.get("/dev/token")
+    async def auth_via_dev(user_id: Optional[str] = None) -> Token:
+        user_id = user_id or settings.DEV_AUTH_EMAIL
+        return create_access_token(user_id)
--- src/app/auth/innopolis.py
+++ src/app/auth/innopolis.py
@@ -1,6 +1,5 @@
 __all__ = []
 
-from fastapi import status, HTTPException
 from starlette.requests import Request
 
 from src.app.auth import router, oauth
@@ -20,25 +19,14 @@ if enabled:
         client_kwargs={"scope": "openid"},
     )
 
+    @router.get("/innopolis/login")
+    async def login_via_innopolis(request: Request):
+        check_enabled()
+        return await oauth.innopolis.authorize_redirect(request, redirect_uri)
 
-def check_enabled():
-    if not enabled:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Auth provider is not enabled",
-        )
-
-
-@router.get("/innopolis/login")
-async def login_via_innopolis(request: Request):
-    check_enabled()
-    return await oauth.innopolis.authorize_redirect(request, redirect_uri)
-
-
-@router.get("/innopolis/token")
-async def auth_via_innopolis(request: Request) -> Token:
-    check_enabled()
-    token = await oauth.innopolis.authorize_access_token(request)
-    user_info = token["userinfo"]
-    email = user_info["email"]
-    return create_access_token(email)
+    @router.get("/innopolis/token")
+    async def auth_via_innopolis(request: Request) -> Token:
+        token = await oauth.innopolis.authorize_access_token(request)
+        user_info = token["userinfo"]
+        email = user_info["email"]
+        return create_access_token(email)
