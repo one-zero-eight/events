c5581f4ea2292180630ef81988c02a4db193e259
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2023-07-07 17:51:09 +0300

    chore(api/schemas-registry): remove schemas-registry router
    BREAKING-CHANGE: schemas-router is removed

--- src/app/routers.py
+++ src/app/routers.py
@@ -1,8 +1,7 @@
-from src.app.schemas_registry import router as router_schemas_registry
 from src.app.users import router as router_users
 from src.app.event_groups import router as router_event_groups
 from src.app.auth import router as router_auth
 
-routers = [router_users, router_event_groups, router_schemas_registry, router_auth]
+routers = [router_users, router_event_groups, router_auth]
 
 __all__ = ["routers", *routers]
