0037a81b6d580db29a0c55710221bd1d75cb59d0
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2023-07-09 20:33:35 +0300

    test(repositories): fix errors with fixtures

--- tests/repositories/test_users.py
+++ tests/repositories/test_users.py
@@ -1,49 +1,54 @@
 import pytest
-import unittest
+import pytest_asyncio
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.config import settings
-from src.repositories.users import SqlUserRepository
+from src.config import Settings, settings as config_settings
+from src.repositories.users import SqlUserRepository, AbstractUserRepository
 from src.schemas.users import CreateUser, ViewUser
-from src.storages.sql import SQLAlchemyStorage
+from src.storages.sql import SQLAlchemyStorage, AbstractSQLAlchemyStorage
 
 
-class TestUsers(unittest.IsolatedAsyncioTestCase):
-    @pytest.fixture(scope="package", autouse=True)
-    def _set_settings(self):
-        self.settings = settings
+@pytest.fixture(scope="package")
+def settings() -> "Settings":
+    return config_settings
 
-    @pytest.fixture(scope="package", autouse=True)
-    def _set_storage(self):
-        _storage = SQLAlchemyStorage.from_url(self.settings.DB_URL.get_secret_value())
-        self.storage = _storage
 
-    @pytest.fixture(scope="package", autouse=True)
-    def repository(self):
-        _repository = SqlUserRepository(self.storage)
-        self.repository = _repository
+@pytest.fixture(scope="package")
+def storage(settings: "Settings") -> "AbstractSQLAlchemyStorage":
+    _storage = SQLAlchemyStorage.from_url(settings.DB_URL.get_secret_value())
+    return _storage
 
-    @pytest.fixture(autouse=True)
-    async def setup(self):
-        from sqlalchemy.sql import text
 
-        # Clear all data from the database before each test
-        async with self.storage.create_session() as session:
-            session: AsyncSession
-            q = text("DROP SCHEMA public CASCADE;" "CREATE SCHEMA public;")
-            await session.execute(q)
-        # Create the necessary tables before each test
-        await self.storage.create_all()
-        yield
-        # Close the database connection after each test
-        await self.storage.close_connection()
+@pytest.fixture(scope="package")
+def repository(storage) -> "AbstractUserRepository":
+    return SqlUserRepository(storage)
 
-    @pytest.mark.asyncio
-    async def test_create_if_not_exists(self):
-        user_schema = CreateUser(email="test@innopolis.university", name="Test User")
-        user = await self.repository.create_user_if_not_exists(user_schema)
-        self.assertIsNotNone(user)
-        self.assertIsInstance(user, ViewUser)
-        self.assertIsNotNone(user.id)
-        self.assertEqual(user.email, user_schema.email)
-        self.assertEqual(user.name, user_schema.name)
+
+@pytest_asyncio.fixture(scope="function", autouse=True)
+async def setup_storage(storage: "AbstractSQLAlchemyStorage"):
+    from sqlalchemy.sql import text
+
+    # Clear all data from the database before each test
+    async with storage.create_session() as session:
+        session: AsyncSession
+        q = text("DROP SCHEMA public CASCADE;")
+        await session.execute(q)
+        q = text("CREATE SCHEMA public;")
+        await session.execute(q)
+
+    # Create the necessary tables before each test
+    await storage.create_all()
+    yield
+    # Close the database connection after each test
+    await storage.close_connection()
+
+
+@pytest.mark.asyncio
+async def test_create_if_not_exists(repository):
+    user_schema = CreateUser(email="test@innopolis.university", name="Test User")
+    user = await repository.create_user_if_not_exists(user_schema)
+    assert user is not None
+    assert isinstance(user, ViewUser)
+    assert user.id is not None
+    assert user.email == user_schema.email
+    assert user.name == user_schema.name
