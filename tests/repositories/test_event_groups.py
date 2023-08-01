1e58591eab489df1408b7538b525de7b8258e98f
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2023-08-01 13:56:22 +0300

    tests: add tests for event group repository

--- tests/repositories/test_event_groups.py
+++ tests/repositories/test_event_groups.py
@@ -56,6 +56,22 @@ async def test_create_if_not_exists(event_group_repository):
 
 
 @pytest.mark.asyncio
+async def test_create_existing(event_group_repository):
+    event_group_schema = get_fake_event_group()
+    event_group = await event_group_repository.create_or_read(event_group_schema)
+    assert event_group is not None
+    assert isinstance(event_group, ViewEventGroup)
+    assert event_group.id is not None
+    assert event_group.name == event_group_schema.name
+
+    event_group = await event_group_repository.create_or_read(event_group_schema)
+    assert event_group is not None
+    assert isinstance(event_group, ViewEventGroup)
+    assert event_group.id is not None
+    assert event_group.name == event_group_schema.name
+
+
+@pytest.mark.asyncio
 async def test_batch_create_if_not_exists(event_group_repository):
     await _batch_create_event_group(event_group_repository)
 
