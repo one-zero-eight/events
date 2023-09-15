e5d74c53ace64fdca1ddc6c006b016a6ac681bab
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2023-09-15 15:42:49 +0300

    refactor: declare mixins before other models

--- src/storages/sql/models/__init__.py
+++ src/storages/sql/models/__init__.py
@@ -1,9 +1,11 @@
 from src.storages.sql.models.base import Base
 
-from src.storages.sql.models.users import User
+import src.storages.sql.models.__mixin__  # noqa: F401, E402
+
 from src.storages.sql.models.event_groups import EventGroup, UserXFavoriteEventGroup
 from src.storages.sql.models.events import Event, EventPatch
 from src.storages.sql.models.tags import Tag
+from src.storages.sql.models.users import User
 from src.storages.sql.models.workshops import Workshop, Timeslot, CheckIn
 
 __all__ = [
